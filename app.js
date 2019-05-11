d08af028c14257e6dbbf50ad506cc8359cc8bb23
haiwenqi <haiwenqi@163.com>
2019-05-11 11:22:20 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+App({
+  onLaunch(options) {
+    console.log('App Launch', options);
+    console.log('getSystemInfoSync', dd.getSystemInfoSync());
+    console.log('SDKVersion', dd.SDKVersion);
+    this.globalData.corpId = options.query.corpId;
+  },
+  onShow() {
+    console.log('App Show');
+  },
+  onHide() {
+    console.log('App Hide');
+  },
+  globalData: {
+    corpId: '',
+    level: '',
+    id: '',
+    domain: 'http://192.168.31.83:8081'
+  }
+});
